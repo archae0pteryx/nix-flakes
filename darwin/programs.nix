e2cb8092428d5aa233938000428ff074f231fc7f
archae0pteryx <github@pocketcereal.com>
2023-11-22 06:57:31 -0800

    update git config, add diff-so-fancy

--- darwin/programs.nix
+++ darwin/programs.nix
@@ -1,10 +1,15 @@
 { systemVars, pkgs, ... }: {
   go.enable = true;
   fish = import ./fish.nix { inherit systemVars pkgs; };
+  gitui = {
+    enable = true;
+  };
   git = {
     enable = true;
     userName = "archae0pteryx";
+    diff-so-fancy.enable = true;
     userEmail = "github@pocketcereal.com";
+    ignores = [ ".DS_Store" "tmp" "node_modules" ".vscode" ];
     extraConfig = {
       pull = { ff = "only"; };
       user = { signingKey = "~/.ssh/id_ed25519.pub"; };
